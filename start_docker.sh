921423af9edd7074bf2ae18b82e79755f201fa41
Hidetoshi Miyazawa <h.miyazawa@seiei-ashd.co.jp>
2025-08-28 18:26:17 +0900

    error log update of wslubuntu

--- start_docker.sh
+++ start_docker.sh
@@ -1,8 +1,14 @@
 docker run --rm -it --name optiserve-backend \
   -p 8000:8000 \
-  -e DATABASE_URL="sqlite:////data/poc_optigate.db" \
-  -e REPORTS_DIR="/data/reports" \
-  -e UPLOADS_DIR="/data/uploads" \
-  -v "$(pwd)/optiserve-backend:/app" \
+  -e DATABASE_URL="sqlite:////app/poc_optigate.db" \
+  -e "$(pwd)/data:/app/data" \
+  -e "$(pwd)/files:/app/files" \
   optiserve-backend:dev
 
+#docker run --rm -it --name optiserve-backend \
+#  -p 8000:8000 \
+#  -e DATABASE_URL="sqlite:////data/poc_optigate.db" \
+#  -e REPORTS_DIR="/data/reports" \
+#  -e UPLOADS_DIR="/data/uploads" \
+#  -v "$(pwd)/optiserve-backend:/app" \
+#  optiserve-backend:dev
